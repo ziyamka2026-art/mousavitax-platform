06335eacba7ffc4af43e9da91c2ea227d75d18d4
پلتفرم جامع مالیاتی موسوی <ziya.mka2026@gmail.com>
2026-09-24 20:27:14 +0330

    feat(tax): audit procedure engine (ماده ۲۱۹ / ۲۰۰/۹۹/۵۲۲) + API + /audit-procedure UI

--- packages/taxlaw-engine/taxlaw_engine/__init__.py
+++ packages/taxlaw-engine/taxlaw_engine/__init__.py
@@ -1,19 +1,39 @@
 from .waiver import (
     WAIVER_VERSION,
     CIRCULAR_CONFIG,
+    DOC_CHECKLIST,
+    DEFAULT_PENALTY_TYPES,
     PenaltyRow,
     WaiverInput,
     WaiverResult,
     calculate_waiver,
     run_smoke_tests,
 )
+from .audit_procedure import (
+    AUDIT_PROC_VERSION,
+    LEGAL_SOURCES,
+    AuditProcedureInput,
+    AuditProcedureResult,
+    calculate_audit_procedure,
+    audit_procedure_meta,
+    run_audit_procedure_smoke,
+)
 
 __all__ = [
     "WAIVER_VERSION",
     "CIRCULAR_CONFIG",
+    "DOC_CHECKLIST",
+    "DEFAULT_PENALTY_TYPES",
     "PenaltyRow",
     "WaiverInput",
     "WaiverResult",
     "calculate_waiver",
     "run_smoke_tests",
+    "AUDIT_PROC_VERSION",
+    "LEGAL_SOURCES",
+    "AuditProcedureInput",
+    "AuditProcedureResult",
+    "calculate_audit_procedure",
+    "audit_procedure_meta",
+    "run_audit_procedure_smoke",
 ]
